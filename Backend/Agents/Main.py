9487da6a26f328b7b211b351e08b16dff282df4e
Sadurshan <167612987+babyele@users.noreply.github.com>
2025-08-29 21:17:50 +0530

    running code

--- Backend/Agents/Main.py
+++ Backend/Agents/Main.py
@@ -6,16 +6,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-app=FastAPI(title="Climatic Policy AI - Policy Comparator")
+app = FastAPI(title="Climatic Policy AI - Policy Comparator")
 
-origins=os.getenv("ALLOWED_ORIGINS","*").split(",")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # Or specify your frontend URL
     allow_credentials=True,
-    allow_methods=["POST"],
+    allow_methods=["*"],  # Must allow OPTIONS for preflight
     allow_headers=["*"],
-
 )
 
 app.include_router(Routes.router, prefix="/api", tags=["Policy Comparator"])
