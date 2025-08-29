c82815e0723959beb69ac1bf2e6d768c18756a77
Sadurshan <167612987+babyele@users.noreply.github.com>
2025-08-29 19:40:09 +0530

    Frontend change

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Navbar /> 
+      <ToastContainer /> {/* Add ToastContainer here */}
+      <Routes>
+        
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
